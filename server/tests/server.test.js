4d60214e160b12686683c8bd95b41bac6ae5c8d0
Alexius Hale-Dubuque <alexiushaledubuque@yahoo.com>
2017-05-06 20:32:46 -0400

    Add test cases for GET /todos/:id

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -1,12 +1,15 @@
 const expect = require('expect')
 const request = require('supertest')
+const { ObjectID } = require('mongodb')
 
 const { app } = require('./../server')
 const { Todo } = require('./../models/todo')
 
 const todos = [{
+	_id: new ObjectID(),
 	text: 'First test todo'
 }, {
+	_id: new ObjectID(),
 	text: 'Second test todo'
 }]
 
@@ -72,4 +75,36 @@ describe('GET /todos', () => {
 			})
 			.end(done)
 	})
+})
+
+describe('GET /todos/:id', () => {
+	it('should return todo doc', (done) => { // async test so done is specified
+		request(app)
+			.get(`/todos/${todos[0]._id.toHexString()}`) // must pass strings in get request
+			.expect(200)
+			.expect((res) => {
+				expect(res.body.todo.text).toBe(todos[0].text) // custom expect
+			})
+			.end(done)
+	})
+
+	// CHALLENGE
+	it('should return 404 if todo not found', (done) => {
+		// make a request with a real Object id - valid id
+		// make sure you get a 404 back
+		var hexId = new ObjectID().toHexString()
+
+		request(app)
+			.get(`/todos/${hexId}`)
+			.expect(404)
+			.end(done)
+	})
+
+	it('should return 404 for non-object ids', (done) => {
+		// /todos/123 (unable to convert to an object id with toHexString() 
+		request(app)
+			.get('/todos/123abc')
+			.expect(404)
+			.end(done)
+	}) 
 })
