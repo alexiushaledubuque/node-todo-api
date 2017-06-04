5bc60ed09a7de9bbe4401c9b2dbfb9a43e9490e0
Alexius Hale-Dubuque <alexiushaledubuque@yahoo.com>
2017-06-04 13:57:30 -0400

    Deploying to Heroku

--- server/server.js
+++ server/server.js
@@ -70,7 +70,7 @@ app.get('/todos/:id', (req, res) => {
 })
 
 app.listen(port, () => {
-	console.log(`Started on port ${port}`)
+	console.log(`Started up at port ${port}`)
 })
 
 // exported for testing purposes
