8db56b83b8a3a8050ee3eb53815a5616a1785a31
Alexius Hale-Dubuque <alexiushaledubuque@yahoo.com>
2017-06-04 18:39:30 -0400

    Add PATCH /todos/:id route

--- server/server.js
+++ server/server.js
@@ -1,4 +1,5 @@
 // Library imports
+const _ = require('lodash')
 const express = require('express')
 const bodyParser = require('body-parser')
 const { ObjectID } = require('mongodb')
@@ -94,8 +95,36 @@ app.delete('/todos/:id', (req, res) => {
 			res.status(400).send()
 		})
 })
-  	
-  	
+
+app.patch('/todos/:id', (req, res) => {
+	var id = req.params.id
+
+	// a subset of the things the user passed to us - prevents the user from
+	// updating anything they choose
+	var body = _.pick(req.body, ['text', 'completed'])
+
+	if (!ObjectID.isValid(id)) {	
+		return res.status(404).send()
+	}
+
+	// updated completedAt based off the completed property
+	if (_.isBoolean(body.completed) && body.completed) {
+		body.completedAt = new Date().getTime()
+	} else {
+		body.completed = false
+		body.completedAt = null
+	}
+
+	Todo.findByIdAndUpdate(id, {$set: body}, {new: true}).then((todo) => {
+		if (!todo) {
+			return res.status(404).send()
+		}
+
+		res.send({todo})
+	}).catch((e) => {
+		res.status(400).send()
+	})
+}) 	
 
 app.listen(port, () => {
 	console.log(`Started up at port ${port}`)
